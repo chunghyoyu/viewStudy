e4a672c5b3083a9b5a0dfabfb5f599e8b89d492d
chunghyoyu <81069077+chunghyoyu@users.noreply.github.com>
2022-01-23 16:48:30 +0900

    Add files via upload

---
+++ 01_Day/01_ES6리뷰/module/a.js
@@ -0,0 +1,11 @@
+// a.jsp
+
+export var num = 10;
+
+export function fun(){
+    return "fun";
+}
+
+export class Person{
+
+}
---
+++ 01_Day/01_ES6리뷰/module/b.js
@@ -0,0 +1,9 @@
+//b.js
+
+// export default () => {
+//     console.log("arrow fun");
+// }
+
+// export default str = "hello";
+const str = "HELLO";
+export default str;
